21ed32e2dd5ee1e82e4a391ea3976334cedde957
Michael Wang <tjcuwang@yahoo.com>
2016-05-02 21:47:18 +0800

    GUI integrated with Monitor services

--- aedms-wf-gui/app/scripts/controllers/monitor.js
+++ aedms-wf-gui/app/scripts/controllers/monitor.js
@@ -1,48 +1,53 @@
-angular.module('activitiApp').controller('MonitorCtrl', function ($scope, $rootScope, $location,  $modal, moment) {
+angular.module('activitiApp').controller('MonitorCtrl', function ($scope, $rootScope, $location,  $modal, moment, MonitorBAMService) {
     if (typeof  $rootScope.loggedin == 'undefined' || $rootScope.loggedin == false) {
         $location.path('/login');
         return;
     }
 
-    $scope.options = {
-      chart: {
-        type: 'discreteBarChart',
-        height: 450,
-        margin : {
-            top: 20,
-            right: 20,
-            bottom: 60,
-            left: 55
-        },
-        x: function(d){ return d.label; },
-        y: function(d){ return d.value; },
-        showValues: true,
-        valueFormat: function(d){
-            return d3.format(',.4f')(d);
-        },
-        transitionDuration: 500,
-        xAxis: {
-            axisLabel: 'X Axis'
-        },
-        yAxis: {
-            axisLabel: 'Y Axis',
-            axisLabelDistance: 30
-        }
-    }
-};
+    
+     MonitorBAMService.get(function(response) {
+      
+         var engineFleetKPI = response
+         var engineFleetArray = [{"label" : "A" , "value" : -29.765957771107}];
 
-$scope.data = [{
-    key: "Cumulative Return",
-    values: [
-        { "label" : "A" , "value" : -29.765957771107 },
-        { "label" : "B" , "value" : 0 },
-        { "label" : "C" , "value" : 32.807804682612 },
-        { "label" : "D" , "value" : 196.45946739256 },
-        { "label" : "E" , "value" : 0.19434030906893 },
-        { "label" : "F" , "value" : -98.079782601442 },
-        { "label" : "G" , "value" : -13.925743130903 },
-        { "label" : "H" , "value" : -5.1387322875705 }
-    ]
-}];
+         console.log("Get Engines KPI " + JSON.stringify(engineFleetKPI));
+         
+         for(var index in engineFleetKPI) {
+            engineFleetArray.push({"label" : index, "value" : engineFleetKPI[index]})
+         }
+
+        $scope.options = {
+          chart: {
+           type: 'discreteBarChart',
+           height: 450,
+           margin : {
+              top: 20,
+              right: 20,
+              bottom: 60,
+              left: 55
+           },
+           x: function(d){ return d.label; },
+           y: function(d){ return d.value; },
+           showValues: true,
+           valueFormat: function(d){
+             return d3.format(',.4f')(d);
+           },
+           transitionDuration: 500,
+           xAxis: {
+              axisLabel: 'X Axis'
+           },
+           yAxis: {
+              axisLabel: 'Y Axis',
+              axisLabelDistance: 30
+           }
+         }
+        };
+         
+        $scope.data = [{
+          key: "Engine Fleet Realtime Monitor",
+          values: engineFleetArray
+        }];
+         
+    });
 
 });
