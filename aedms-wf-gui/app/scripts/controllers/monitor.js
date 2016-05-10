fc5f8f3afa1913e05cc058b0d44f225a17ac41c5
Michael Wang <tjcuwang@yahoo.com>
2016-05-10 21:55:15 +0800

    Websocket KPI working now

--- aedms-wf-gui/app/scripts/controllers/monitor.js
+++ aedms-wf-gui/app/scripts/controllers/monitor.js
@@ -1,24 +1,15 @@
-angular.module('activitiApp').controller('MonitorCtrl', function ($scope, $rootScope, $location,  $modal, moment, MonitorBAMService) {
+angular.module('activitiApp').controller('MonitorCtrl', function ($scope, $interval, $rootScope, $location,  $modal, moment, MonitorBAMService) {
     if (typeof  $rootScope.loggedin == 'undefined' || $rootScope.loggedin == false) {
         $location.path('/login');
         return;
     }
 
-    $scope.socket = {
+    var socket = {
         client: null,
         stomp: null
     };
     
-    $scope.initSockets = function() {
-        $scope.socket.client = new SockJS('http://localhost:8083/aedms-monitor/engine-bam-ed');
-        $scope.socket.stomp = Stomp.over($scope.socket.client);
-        $scope.socket.stomp.connect({}, function() {
-            $scope.socket.stomp.subscribe("/topic/engine-bam-ws", 
-                   function(message){
-
-                     console.log(JSON.parse(message.body));
-                     var engineFleetArray = JSON.parse(message.body);
-                     $scope.options = {
+    $scope.options = {
                        chart: {
                          type: 'discreteBarChart',
                          height: 450,
@@ -43,57 +34,105 @@ angular.module('activitiApp').controller('MonitorCtrl', function ($scope, $rootS
                             axisLabelDistance: 30
                        }
                      }
-                   };
-         
-                 $scope.data = [{
+                  };
+
+     $scope.data = [{
                       key: "Engine Fleet Realtime Monitor",
-                      values: engineFleetArray
+                      values: [{"label":"HP", "value": 1}]
                  }];
+
+    
+    initSockets = function() {
+        socket.client = new SockJS('http://localhost:8083/aedms-monitor/engine-bam-ed');
+        socket.stomp = Stomp.over(socket.client);
+        socket.stomp.connect({}, function() {
+        socket.stomp.subscribe("/topic/engine-bam-ws", 
+            function(message){
+
+                     console.log(JSON.parse(message.body));
+
+                     var engineFleetArray = JSON.parse(message.body);
+
+                     function update() {
+                           $scope.options = {
+                                chart: {
+                                   type: 'discreteBarChart',
+                                   height: 450,
+                                   margin : {
+                                      top: 20,
+                                      right: 20,
+                                      bottom: 60,
+                                      left: 55
+                               },
+                               x: function(d){ return d.label; },
+                               y: function(d){ return d.value; },
+                               showValues: true,
+                               valueFormat: function(d){
+                                 return d3.format(',.4f')(d);
+                               },
+                               transitionDuration: 500,
+                               xAxis: {
+                                  axisLabel: 'X Axis'
+                               },
+                               yAxis: {
+                                  axisLabel: 'Y Axis',
+                                  axisLabelDistance: 30
+                               }
+                              }
+                          };
+                          $scope.data = [{
+                              key: "Engine Fleet Realtime Monitor",
+                             values: engineFleetArray
+                          }];
+                    }
+
+                    $interval(update, 1000);
+                
 
              });
         });
 
-        $scope.socket.client.onclose = $scope.reconnect;
     };
     
-    $scope.initSockets();
+    initSockets();
     
-    MonitorBAMService.query(function(response) {
+    // Comment out following Restful way.
+    // MonitorBAMService.query(function(response) {
       
-         var engineFleetArray =response;
+    //      var engineFleetArray =response;
 
-        $scope.options = {
-          chart: {
-           type: 'discreteBarChart',
-           height: 450,
-           margin : {
-              top: 20,
-              right: 20,
-              bottom: 60,
-              left: 55
-           },
-           x: function(d){ return d.label; },
-           y: function(d){ return d.value; },
-           showValues: true,
-           valueFormat: function(d){
-             return d3.format(',.4f')(d);
-           },
-           transitionDuration: 500,
-           xAxis: {
-              axisLabel: 'X Axis'
-           },
-           yAxis: {
-              axisLabel: 'Y Axis',
-              axisLabelDistance: 30
-           }
-         }
-        };
+    //     $scope.options = {
+    //       chart: {
+    //        type: 'discreteBarChart',
+    //        height: 450,
+    //        margin : {
+    //           top: 20,
+    //           right: 20,
+    //           bottom: 60,
+    //           left: 55
+    //        },
+    //        x: function(d){ return d.label; },
+    //        y: function(d){ return d.value; },
+    //        showValues: true,
+    //        valueFormat: function(d){
+    //          return d3.format(',.4f')(d);
+    //        },
+    //        transitionDuration: 500,
+    //        xAxis: {
+    //           axisLabel: 'X Axis'
+    //        },
+    //        yAxis: {
+    //           axisLabel: 'Y Axis',
+    //           axisLabelDistance: 30
+    //        }
+    //      }
+    //     };
          
-        $scope.data = [{
-          key: "Engine Fleet Realtime Monitor",
-          values: engineFleetArray
-        }];
+    //     $scope.data = [{
+    //       key: "Engine Fleet Realtime Monitor",
+    //       values: engineFleetArray
+    //     }];
          
-    });
+    // });
 
 });
